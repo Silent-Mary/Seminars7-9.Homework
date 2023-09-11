c923939aeb74777bc59e0b326d68c741eccc80e8
SilentMary <playinglassbead@gmail.com>
2023-09-11 21:43:44 +0100

    47 done

--- Task 47/Program.cs
+++ Task 47/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*  Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9 */
+
+Console.Clear();
+
+double[,] GetRandomMatrix(int row, int column, double start, double end)
+{
+    double[,] matrix = new double[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            matrix[i, j] = Math.Round((new Random().NextDouble() * (end - start) + start), 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrixToPrint)
+{
+    System.Console.Write($"[ ]\t");
+    for (int i = 0; i < matrixToPrint.GetLength(1); i++)
+    {
+        Console.Write($"[{i}]\t");
+    }
+    Console.WriteLine();
+
+    for (int i = 0; i < matrixToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[{i}]\t");
+         for (int j = 0; j < matrixToPrint.GetLength(1); j++)
+         {
+            System.Console.Write(matrixToPrint[i, j] +"\t");  
+         } 
+    Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Enter N of rows, N of columns, min N & max N for range: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+int columns = Convert.ToInt32(Console.ReadLine());
+double min = Double.Parse(Console.ReadLine());
+double max = Double.Parse(Console.ReadLine());
+
+double[,] userMatrix = GetRandomMatrix(rows, columns, min, max);
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Green;
+PrintMatrix(userMatrix);
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.White;
