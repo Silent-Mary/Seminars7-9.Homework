1b0ae500aaa9de878e7a80f378cf9390033bb364
SilentMary <playinglassbead@gmail.com>
2023-09-18 19:33:46 +0100

    this was hell

--- Task 58/Program.cs
+++ Task 58/Program.cs
@@ -43,33 +43,42 @@ void PrintMatrix(int[,] matrixToPrint)
 
 int[,] MultiplyMatrix (int[,] matrix1, int[,] matrix2)
 {
-    int rows = matrix1.GetLength(0);
-    int columns = matrix1.GetLength(1);
-    int[,] product = new int[rows, columns];
-    for (int row = 0; row < rows; row++)
+    int rows1 = matrix1.GetLength(0);
+    int columns1rows2 = matrix1.GetLength(1);
+    // int rows2 = matrix2.GetLength(0);
+    int columns2 = matrix2.GetLength(1);
+    int[,] product = new int[rows1, columns1rows2];
+    // int multElements = 1;
+    for (int i = 0; i < rows1; i++)
     {
-        for (int column = 0; column < columns; column++)
+        for (int j = 0; j < columns1rows2; j++)
         {
-            product[row, column] = matrix1[row, column] * matrix2[row, column];
+            product[i, j] = 0;
+            for (int k = 0; k < 2; k++)
+            {
+               product[i, j] += matrix1[i, k] * matrix2[k, j];
+            }
         }
     }
     return product;
 }
 
 Console.WriteLine("Enter N of rows, N of columns, min N & max N for range: ");
-int rows = Convert.ToInt32(Console.ReadLine());
-int columns = Convert.ToInt32(Console.ReadLine());
+int rows1 = Convert.ToInt32(Console.ReadLine());
+int columns1 = Convert.ToInt32(Console.ReadLine());
 int min1 = Convert.ToInt32(Console.ReadLine());
 int max1 = Convert.ToInt32(Console.ReadLine());
 Console.Clear();
 
-Console.WriteLine("Enter min & max range for the 2nd matrix: ");
+Console.WriteLine("Enter N of columns, min & max range for the 2nd matrix: ");
+int rows2 = columns1;
+int columns2 = Convert.ToInt32(Console.ReadLine());
 int min2 = Convert.ToInt32(Console.ReadLine());
 int max2 = Convert.ToInt32(Console.ReadLine());
 Console.Clear();
 
-int[,] userMatrix1 = GetRandomMatrix(rows, columns, min1, max1);
-int[,] userMatrix2 = GetRandomMatrix(rows, columns, min2, max2);
+int[,] userMatrix1 = GetRandomMatrix(rows1, columns1, min1, max1);
+int[,] userMatrix2 = GetRandomMatrix(rows2, columns2, min2, max2);
 Console.ForegroundColor = ConsoleColor.Cyan;
 PrintMatrix(userMatrix1);
 Console.WriteLine();
