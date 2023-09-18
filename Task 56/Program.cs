4ce0efdd0f9809fa63a888f5351bb980b04a8423
SilentMary <playinglassbead@gmail.com>
2023-09-18 16:38:11 +0100

    task 56

---
+++ Task 56/Program.cs
@@ -0,0 +1,83 @@
+﻿/* Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка */ 
+
+Console.Clear();
+
+int[,] GetRandomMatrix(int row, int column, int start, int end)
+{
+    int[,] matrix = new int[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            matrix[i, j] = new Random().Next(start, end+1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrixToPrint)
+{
+    System.Console.Write($"[ ]\t");
+    for (int i = 0; i < matrixToPrint.GetLength(1); i++)
+    {
+        Console.Write($"[{i}]\t");
+    }
+    Console.WriteLine();
+
+    for (int i = 0; i < matrixToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[{i}]\t");
+         for (int j = 0; j < matrixToPrint.GetLength(1); j++)
+         {
+            System.Console.Write(matrixToPrint[i, j] +"\t");  
+         } 
+    Console.WriteLine();
+    }
+}
+
+int SingleRowSum(int[,] matrix, int rows)  
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        sum += matrix[rows, i];
+    }
+    return sum;
+}
+
+int SmallestSumRow(int[,] matrix)
+{
+    int rows = 0;
+    int min = SingleRowSum(matrix, 0);
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        if (SingleRowSum(matrix, i) < min)
+        {
+            rows = i;
+            min = SingleRowSum(matrix, i);
+        }
+    }
+    return rows;
+}
+
+Console.WriteLine("Enter N of rows, N of columns, min N & max N for range: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+int columns = Convert.ToInt32(Console.ReadLine());
+int min = Convert.ToInt32(Console.ReadLine());
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] userMatrix = GetRandomMatrix(rows, columns, min, max);
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Cyan;
+PrintMatrix(userMatrix);
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine();
+Console.WriteLine($"Row ["+SmallestSumRow(userMatrix)+"] has the smallest sum.");
+Console.WriteLine();
+
