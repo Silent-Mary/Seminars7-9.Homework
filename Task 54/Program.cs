fc243a6c9864e30b24c969cc6c06ab8607cf7e76
SilentMary <playinglassbead@gmail.com>
2023-09-18 16:22:59 +0100

    finally doing my homework

---
+++ Task 54/Program.cs
@@ -0,0 +1,88 @@
+﻿/* Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2 */ 
+
+Console.Clear();
+
+int[,] GetRandomMatrix(int row, int column, int start, int end)
+{
+    int[,] matrix = new int[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            matrix[i, j] = new Random().Next(start, end+1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrixToPrint)
+{
+    System.Console.Write($"[ ]\t");
+    for (int i = 0; i < matrixToPrint.GetLength(1); i++)
+    {
+        Console.Write($"[{i}]\t");
+    }
+    Console.WriteLine();
+
+    for (int i = 0; i < matrixToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[{i}]\t");
+         for (int j = 0; j < matrixToPrint.GetLength(1); j++)
+         {
+            System.Console.Write(matrixToPrint[i, j] +"\t");  
+         } 
+    Console.WriteLine();
+    }
+}
+
+int[,] singleRowSorting(int[,] matrix, int rows)
+{
+    int temp = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+        {
+            if (matrix[rows, j] < matrix[rows, j + 1])
+            {
+                temp = matrix[rows, j];
+                matrix[rows, j] = matrix[rows, j + 1];
+                matrix[rows, j + 1] = temp;
+            }
+        }
+    }
+    return matrix;
+}
+
+int[,] jumpRows(int[,] matrix) 
+{
+    for (int rows = 0; rows < matrix.GetLength(0); rows++)
+    {
+       singleRowSorting(matrix, rows);
+    }
+    return matrix;
+}
+
+Console.WriteLine("Enter N of rows, N of columns, min N & max N for range: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+int columns = Convert.ToInt32(Console.ReadLine());
+int min = Convert.ToInt32(Console.ReadLine());
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] userMatrix = GetRandomMatrix(rows, columns, min, max);
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Cyan;
+PrintMatrix(userMatrix);
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Green;
+PrintMatrix(jumpRows(userMatrix));
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine();
